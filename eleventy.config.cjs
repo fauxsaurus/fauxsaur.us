2986a512b03b9f5fb8452154b9590301c7141573
minifyre <31806063+minifyre@users.noreply.github.com>
2024-02-21 12:29:20 -0500

    +11ty
    * Added dependencies
    * Added some custom filters to fill in the gaps between old jekyll behavior and new 11ty behavior (i.e., permalinks, kebab case, etc.)

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -1,3 +1,23 @@
-module.exports = (eleventyConfig) => {
-	return {};
-};
+module.exports = eleventyConfig => {
+	/** @todo concat if multiple scripts */
+	eleventyConfig.addPassthroughCopy('src/js/*.js')
+	eleventyConfig.addPassthroughCopy('src/font/*.ttf')
+
+	/** @note _prefix denotes that these are custom filters that should hopefully not interfere with others if newer filters are added with similar names */
+	/** @note this could cause issues if a person's hyphenated last name is ever used as a page name. */
+	eleventyConfig.addFilter('_kebab2titleCase', function (string) {
+		return string
+			.split('-')
+			.map(
+				([letter, ...letters]) =>
+					letter.toLocaleUpperCase(this.page.lang) + letters.join('')
+			)
+			.join(' ')
+	})
+
+	eleventyConfig.addFilter('_post2permalink', ([lang, word4blog, date, slug]) => {
+		const [yyyy, mm, dd] = date.toISOString().split('T')[0].split('-')
+		return ['', lang, word4blog, yyyy, mm, dd, slug, 'index.html'].join('/')
+	})
+
+}
