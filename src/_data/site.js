ce5deb2921dfba9ce6c02455f201b191a2fe4c9a
minifyre <31806063+minifyre@users.noreply.github.com>
2024-02-21 13:47:45 -0500

    Changed Analytics
    Replaced Google Analytics with privacy-friendly Cloudflare Web Analytics.

--- src/_data/site.js
+++ src/_data/site.js
@@ -2,7 +2,7 @@ module.exports = () => {
 	const isProduction = process.env.MY_ENVIRONMENT === 'production'
 
 	return {
-		analytics: '85283400-1',
+		analytics: 'a0f89da9564c4e919cd40568e3934d86',
 		description: '',
 		title: 'Minifyre',
 		url: isProduction ? 'https://minify.re' : 'http://localhost:8080',
