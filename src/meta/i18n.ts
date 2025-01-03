22b68ab66bf54cd67dea958f42e2f36c3d6bfbb2
fauxsarus <minifyre@gmail.com>
2025-01-02 19:36:04 -0500

    migrated redirect

---
+++ src/meta/i18n.ts
@@ -0,0 +1,27 @@
+export const DEFAULT_LANGUAGE = 'en'
+
+export const LANGUAGES = {
+	en: 'English',
+	es: 'Español', // Spanish
+	zh: '简体中文', // Simplified Chinese
+}
+
+export type ISupportedLanguages = keyof typeof LANGUAGES
+
+export const NAV_BAR = {
+	en: {
+		// blog: 'blog',
+		// apps
+		// books: 'books',
+		// music
+		// games
+		about: 'about',
+	},
+}
+
+export const ACCESSIBILITY = {
+	en: {
+		emailIssues: 'Note: if you run into any issues on this site, please email ',
+		skipTo: 'Skip To Content',
+	},
+}
